60bfac1732e7b610b9b8fdc8925120b9a7bc1e21
Peter Klement <peter.klement@broadcom.com>
2022-02-02 12:04:16 +0100

    setInterval

--- src/index.js
+++ src/index.js
@@ -1,39 +1,44 @@
 const { Kafka } = require("kafkajs");
 const config = require("./config");
 const createConsumer = require("./consumer");
-const express = require('express');
+const express = require("express");
 const app = express();
-const http = require('http');
+const http = require("http");
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
+const connectedSockets = new Map();
 
-const connectedSockets = [];
-
-app.get('/', (req,res) => {
-    res.sendFile(__dirname + '/index.html');
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/index.html");
 });
 
-app.get('/send', (req, res) => {
-   if (req.query.msg) {
-       io.emit('notification', { notification: req.query.msg });
-   }
-   res.send('message sent');
+app.get("/send", (req, res) => {
+    if (req.query.msg) {
+        io.emit("notification", { notification: req.query.msg });
+    }
+    res.send("message sent");
 });
 
-io.on('connection', (socket) => {
-    console.log(socket.id + ' user connected');
-    connectedSockets[socket.id] = socket;
-    socket.on('disconnect', () => {
-       console.log(socket.id + ' user disconnected');
-       connectedSockets[socket.id] = undefined;
+io.on("connection", (socket) => {
+    connectedSockets.set(socket.id, socket);
+
+    socket.on("disconnect", () => {
+        console.log(socket.id + " user disconnected");
+        connectedSockets.delete(socket.id);
     });
+
+    console.log(socket.id + " user connected");
 });
 
-
+setInterval(() => {
+    connectedSockets.forEach((socket, id) => {
+        socket.emit("pong", "pong " + id + " " + new Date().valueOf());
+    });
+}, 5000);
 
 server.listen(3000, () => {
-    console.log('listening on *:3000');
+    console.log("listening on *:3000");
 });
 
 /*
